530b38a5fe0838405fd5b4f65ed846b9b55bef4d
Yael Brown <yaelrbrown@gmail.com>
2019-09-06 21:29:06 -0500

    Added cpp helloWorld program.

---
+++ 01_MyFirstProgramCPP/main.cpp
@@ -0,0 +1,23 @@
+// This is my first C++ Program!!
+#include <iostream>
+using namespace std;
+
+int main() {
+	//std::cout << "Hello World!\n";
+
+	int x = 0;
+	int cars = 14;
+	int debt = -1000;
+	float cash = 2.32;
+	double credit = 32.32;
+	char a = 'a';
+	char* sand = "asdf";  // creates a string of characters
+	bool isLikeMe = true;
+	auto havePantsOn = false;
+	auto blank_check = 200.00;
+	int over_9000 = INT_MAX;
+
+	cout << over_9000 << endl;
+
+	return 0;
+}
